3f108cf30d40dba28ffbc4dddf140106533a64e9
Ajay Gandhi <aag255@cornell.edu>
2018-02-08 14:26:50 -0800

    Put element selectors in separate obj

--- web/main.js
+++ web/main.js
@@ -2,19 +2,15 @@ let DURATION = 1, START = 0, END = 1, WAIT = 0;
 let playing = false;
 
 // Grab elements
-const firstPane = document.getElementsByClassName("Pane")[0];
-const selectFile = document.getElementById("selectFile");
-const selectFileInput = document.getElementById("selectFileInput");
-const audio = document.getElementById("audio");
-const timeSelector = document.getElementById("timeSelector");
-const selectedTime = document.getElementById("selectedTime");
-const togglePlayback = document.getElementById("togglePlayback");
-const waitBetweenRepeat = document.getElementById("waitBetweenRepeat");
-const fileTitle = document.getElementById("fileTitle");
-const fileDuration = document.getElementById("fileDuration");
+const elementIds = ["selectFile", "selectFileInput", "audio", "timeSelector", "selectedTime", "togglePlayback", "waitBetweenRepeat", "fileTitle", "fileDuration"];
+const SELECTORS = elementIds.reduce((memo, id) => {
+  memo[id] = document.getElementById(id);
+  return memo;
+}, {});
+SELECTORS.firstPane = document.getElementsByClassName("Pane")[0];
 
 // Setup slider
-noUiSlider.create(timeSelector, {
+noUiSlider.create(SELECTORS.timeSelector, {
   start: [0, 1],
   connect: true,
   range: {
@@ -22,21 +18,21 @@ noUiSlider.create(timeSelector, {
     "max": [1],
   },
 });
-timeSelector.noUiSlider.on("change", () => {
-  const values = timeSelector.noUiSlider.get();
+SELECTORS.timeSelector.noUiSlider.on("change", () => {
+  const values = SELECTORS.timeSelector.noUiSlider.get();
   audio.currentTime = START;
   START = parseFloat(values[0]).toFixed(2);
   END = parseFloat(values[1]).toFixed(2);
 });
-timeSelector.noUiSlider.on("update", () => {
-  const values = timeSelector.noUiSlider.get();
+SELECTORS.timeSelector.noUiSlider.on("update", () => {
+  const values = SELECTORS.timeSelector.noUiSlider.get();
   const s = parseFloat(values[0]).toFixed(2);
   const e = parseFloat(values[1]).toFixed(2);
-  selectedTime.textContent = formatTime(s) + " - " + formatTime(e);
+  SELECTORS.selectedTime.textContent = formatTime(s) + " - " + formatTime(e);
 });
 
 const updateSlider = () => {
-  timeSelector.noUiSlider.updateOptions({
+  SELECTORS.timeSelector.noUiSlider.updateOptions({
     start: [0, DURATION],
     range: {
       "min": [0],
@@ -47,55 +43,55 @@ const updateSlider = () => {
 
 // File upload actions
 const console_log = s => eel.console_log(JSON.stringify(s));
-selectFileInput.addEventListener("change", () => {
-  if (!selectFileInput.files[0]) return;
+SELECTORS.selectFileInput.addEventListener("change", () => {
+  if (!SELECTORS.selectFileInput.files[0]) return;
 
-  const file = selectFileInput.files[0];
-  if (audio.src) URL.revokeObjectURL(audio.src);
-  audio.src = URL.createObjectURL(file);
+  const file = SELECTORS.selectFileInput.files[0];
+  if (SELECTORS.audio.src) URL.revokeObjectURL(SELECTORS.audio.src);
+  SELECTORS.audio.src = URL.createObjectURL(file);
 
-  fileTitle.textContent = file.name;
-  firstPane.classList.add("Pane--advanced");
+  SELECTORS.fileTitle.textContent = file.name;
+  SELECTORS.firstPane.classList.add("Pane--advanced");
 });
 
 // Audio actions
-togglePlayback.addEventListener("click", togglePlaying);
+SELECTORS.togglePlayback.addEventListener("click", togglePlaying);
 window.addEventListener("keydown", (e) => {
   if (e.keyCode === 32) togglePlaying();
 });
 function togglePlaying () {
   if (playing) {
     playing = false;
-    audio.pause();
-    togglePlayback.textContent = "Play";
+    SELECTORS.audio.pause();
+    SELECTORS.togglePlayback.textContent = "Play";
   } else {
-    audio.play().then(function () {
+    SELECTORS.audio.play().then(function () {
       playing = true;
-      togglePlayback.textContent = "Pause";
+      SELECTORS.togglePlayback.textContent = "Pause";
     }).catch(function (e) {
       console.error("Error playing audio", e);
     });
   }
 };
-waitBetweenRepeat.addEventListener("change", () => {
-  WAIT = parseInt(waitBetweenRepeat.value, 10) * 1000;
+SELECTORS.waitBetweenRepeat.addEventListener("change", () => {
+  WAIT = parseInt(SELECTORS.waitBetweenRepeat.value, 10) * 1000;
 });
 
 // Audio events
-audio.addEventListener("durationchange", () => {
-  END = DURATION = audio.duration;
+SELECTORS.audio.addEventListener("durationchange", () => {
+  END = DURATION = SELECTORS.audio.duration;
   START = 0;
-  fileDuration.textContent = formatTime(DURATION);
+  SELECTORS.fileDuration.textContent = formatTime(DURATION);
   updateSlider();
 });
 let repeatTimeout;
-audio.addEventListener("timeupdate", () => {
+SELECTORS.audio.addEventListener("timeupdate", () => {
   if (repeatTimeout) return;
-  if (audio.currentTime > END || audio.currentTime < START) {
-    audio.muted = true;
+  if (SELECTORS.audio.currentTime > END || SELECTORS.audio.currentTime < START) {
+    SELECTORS.audio.muted = true;
     repeatTimeout = setTimeout(() => {
-      audio.muted = false;
-      audio.currentTime = START;
+      SELECTORS.audio.muted = false;
+      SELECTORS.audio.currentTime = START;
       repeatTimeout = null;
     }, WAIT);
   }
